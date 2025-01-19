83d6277c039bbe4b4e238978ea004e2f0c681bdb
Mayuresh Durgule <162131250+mayureshd13@users.noreply.github.com>
2025-01-19 19:10:13 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,23 +1,5 @@
 
-        function autoplayAudio() {
-            // Create a new audio element
-            const audio = new Audio();
-            
-            // Set the source of the audio
-            audio.src = 'Dunki.mp3'; // Replace with your audio file path
-            
-            // Enable looping if needed
-            audio.loop = true;
-
-            // Try to play the audio
-            audio.play().catch(error => {
-              console.log("Autoplay blocked. Interaction may be required:", error);
-            });
-        }
-
-        // Trigger audio play when the button is clicked
-        document.getElementById('playAudioButton').addEventListener('click', autoplayAudio);
-
+        
 
 var radius = 240; 
 var autoRotate = true; 
